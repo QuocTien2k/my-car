c176ea4aa92ed77bcef2c0259b80c410bbe189f3
Do Quoc Tien <hadesdqt@gmail.com>
2026-03-05 14:30:58 +0700

    test(auth): refactor AuthService tests and improve signup validation (part 8)

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -2,26 +2,63 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from 'src/users/users.service';
 import { User } from 'src/users/users.entity';
+import { BadRequestException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
-it('can create an instance of auth service', async () => {
-  //create a fake copy of the users service
-  const fakeUsersService: Partial<UsersService> = {
-    findByEmail: () => Promise.resolve(null),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
+describe('AuthService', () => {
+  let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService,
-      },
-    ],
-  }).compile();
+  beforeEach(async () => {
+    //fake user service
+    fakeUsersService = {
+      findByEmail: () => Promise.resolve(null),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const service = module.get(AuthService);
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
 
-  expect(service).toBeDefined();
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('hashes the password when creating a new user', async () => {
+    const user = await service.signup('nguyenvand@gmail.com', '123456');
+
+    expect(user.password).not.toEqual('123456');
+
+    const isMatch = await bcrypt.compare('123456', user.password);
+    expect(isMatch).toBe(true);
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    //existing user
+    fakeUsersService.findByEmail = (email: string) => {
+      if (email === 'test@test.com') {
+        //thay đổi email để test
+        return Promise.resolve({
+          id: 1,
+          email,
+          password: 'hashed',
+        } as User);
+      }
+      return Promise.resolve(null);
+    };
+
+    await expect(service.signup('test@test.com', '123456')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
 });
