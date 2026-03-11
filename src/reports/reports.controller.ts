d7cf9b88e09fed84a2ca0003640270a9b9ace3c2
Do Quoc Tien <hadesdqt@gmail.com>
2026-03-11 20:57:02 +0700

    feat(reports): inject ReportsService and apply AuthGuard to create report endpoint (part21)

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,8 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('reports')
 export class ReportsController {
+  constructor(private readonly reportsService: ReportsService) {}
+
+  @UseGuards(AuthGuard)
   @Post()
   createReport(@Body() body: CreateReportDto) {}
 }
