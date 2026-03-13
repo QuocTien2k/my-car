98398e054bacc628db383e537005b27fe5c9075a
Do Quoc Tien <hadesdqt@gmail.com>
2026-03-13 21:17:18 +0700

    feat(auth): implement CurrentUserMiddleware and register in AuthModule (part28)

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/users.entity';
 import { Report } from './reports/reports.entity';
 import { AuthModule } from './auth/auth.module';
-import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
-import { CurrentUserInterceptor } from './auth/interceptors/current-user.interceptor';
+import { APP_PIPE } from '@nestjs/core';
 import cookieSession from 'cookie-session';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
@@ -44,10 +43,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor,
-    },
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { CurrentUserMiddleware } from './middleware/current-user.middleware';
 
 @Module({
   imports: [UsersModule], // import UsersModule
   providers: [AuthService],
   controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
